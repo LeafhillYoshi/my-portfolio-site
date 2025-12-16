345baf630860f0579c506c80c987c70eccae6856
Jani Lehtimäki <lehtimaki.e.jani@gmail.com>
2025-12-16 21:26:26 +0200

    Bugfix + color changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,7 +13,7 @@ canvas.style.top = 0;
 canvas.style.left = 0;
 canvas.style.width = '100%';
 canvas.style.height = '100%';
-canvas.style.zIndex = '0';
+canvas.style.zIndex = '-1';
 canvas.style.pointerEvents = 'none';
 
 const ctx = canvas.getContext('2d');
